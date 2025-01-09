3d73f338fe6d24d69e3e9e58ebb613fd147c875c
nah <test@mail.com>
2025-01-09 17:01:00 +0100

    fixing accent colors

--- density-debris/tailwind.config.ts
+++ density-debris/tailwind.config.ts
@@ -29,7 +29,7 @@ export default {
           "@apply underline underline-offset-2": {},
         },
         ".title": {
-          "@apply text-2xl font-semibold text-accent-2": {},
+          "@apply text-2xl font-semibold text-accent": {},
         },
       });
     }),
